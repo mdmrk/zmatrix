cef0b55287eb48b26319ba637b2c4d387e73973b
mdmrk <mariodavo.20@gmail.com>
2025-08-28 02:45:05 +0200

    esc key

--- zmatrix.zig
+++ zmatrix.zig
@@ -304,7 +304,7 @@ fn checkResize(alloc: std.mem.Allocator, tty: *const Tty) !bool {
     return false;
 }
 
-fn kbhit() bool {
+fn keyboardHit() bool {
     const fd = if (windows)
         @as(std.os.windows.ws2_32.SOCKET, @ptrCast(std.os.windows.kernel32.GetStdHandle(std.os.windows.STD_INPUT_HANDLE)))
     else
@@ -317,7 +317,7 @@ fn kbhit() bool {
     return std.posix.poll(@constCast(&[_]std.posix.pollfd{fds}), 0) catch 0 > 0;
 }
 
-fn getch() !u8 {
+fn getChar() !u8 {
     var buffer: [1]u8 = undefined;
     const fd = if (windows)
         @as(std.os.windows.ws2_32.SOCKET, @ptrCast(std.os.windows.kernel32.GetStdHandle(std.os.windows.STD_INPUT_HANDLE)))
@@ -487,14 +487,14 @@ pub fn main() !void {
     const thread = try std.Thread.spawn(.{}, main_loop, .{ alloc, &tty });
 
     while (true) {
-        if (kbhit()) {
-            const key = getch() catch continue;
+        if (keyboardHit()) {
+            const key = getChar() catch continue;
 
             switch (key) {
                 '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' => {
                     args.update_time = getUpdateTime(key);
                 },
-                'q', 'Q' => {
+                'q', 'Q', 27 => {
                     should_stop.store(true, .release);
                     thread.join();
                     break;
